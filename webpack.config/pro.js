b0ea06d7466d79d9c105d1cb60987d6d51c76295
varHarrie <varharrie@gmail.com>
2016-11-24 17:22:49 +0800

    update webpack config for production

--- webpack.config/pro.js
+++ webpack.config/pro.js
@@ -1,14 +1,20 @@
 const path = require('path')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlwebpackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
 
 const baseConfig = require('./base')
 const root = path.resolve(__dirname, '../')
 
 module.exports = merge(baseConfig, {
+  devtool: 'source-map',
   plugins: [
     new HtmlwebpackPlugin({
-      title: 'React Demo'
-    })
+      title: 'React Demo',
+      template: path.join(root, 'templates/index.ejs'),
+      inject: 'body'
+    }),
+    new UglifyJsPlugin({minimize: true})
   ]
 })

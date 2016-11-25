6636e00d17178987c076c6a5c76b4adc69ee1fc2
varHarrie <varharrie@gmail.com>
2016-11-25 13:22:57 +0800

    update webpack config for support hot replacement

--- webpack.config/dev.js
+++ webpack.config/dev.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlwebpackPlugin = require('html-webpack-plugin')
 
@@ -6,12 +7,25 @@ const baseConfig = require('./base')
 const root = path.resolve(__dirname, '../')
 
 module.exports = merge(baseConfig, {
+  entry: [
+    'webpack/hot/dev-server',
+    path.join(root, 'src/index.js')
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    colors: true,
+    port: 3000
+  },
   devtool: 'source-map',
   plugins: [
     new HtmlwebpackPlugin({
       title: 'React Demo',
       template: path.join(root, 'templates/index.ejs'),
       inject: 'body'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 })
